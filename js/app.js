41c43591a67a890127c965da3eca2a1a2057abbd
Jonathan Villeda <johnsvill@hotmail.com>
2019-08-03 12:07:51 -0600

    Mejorando jQuery

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
 $("#todos").click(function(evento) {
-    alert("Haz presionado el botón ALL!!!");
+    $("*").addClass("selector");
+});
+$("#elemento").click(function(evento) {
+    $("p").addClass("selector");
+});
+$("#elementoID").click(function(evento) {
+    $("#email").addClass("selector");
+});
+$("#elementoClase").click(function(evento) {
+    $(".ejemplo").addClass("selector");
+});
+$("#elementoInput").click(function(evento) {
+    $("input").addClass("selector");
+});
+$("#ajax").click(function(evento) {
+    $.ajax({
+        url: "https://www.swapi.co/api/planets/?page=2",
+        success: function(respuesta) {
+            console.log(respuesta.results);
+        },
+        error: function() {
+            console.log("Existe un error al realizar la petición");
+        },
+    });
 });
